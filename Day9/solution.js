661c8a453faef227bed9050f78f4778c084a9614
Sebastian Beck - dotSource GmbH <sbc@intranet.dotsource.de>
2022-12-10 00:57:16 +0100

    solve day 9

--- Day9/solution.js
+++ Day9/solution.js
@@ -1,40 +1,65 @@
 const moves = input.split(/\n/);
 
-let head = {x: 0, y: 0};
-let tail = {x: 0, y: 0};
+let rope = [{x:0, y:0}, {x:0, y:0}];
+let rope = [{x:0, y:0}, {x:0, y:0}, {x:0, y:0}, {x:0, y:0}, {x:0, y:0}, {x:0, y:0}, {x:0, y:0}, {x:0, y:0}, {x:0, y:0}, {x:0, y:0}];
+
 let visitedPositions = [{x:0, y:0}];
 let numberOfVisitedPositions = 1;
 
 function moveHead(direction) {
     switch (direction) {
-        case "L": head.x--; break;
-        case "R": head.x++; break;
-        case "U": head.y--; break;
-        case "D": head.y++;
+        case "L": rope[0].x--; break;
+        case "R": rope[0].x++; break;
+        case "U": rope[0].y--; break;
+        case "D": rope[0].y++; break;
     }
 }
 
-function headTouchesTail() {
-    return (head.x === tail.x || head.x-1 === tail.x || head.x+1 === tail.x)
-        && (head.y === tail.y || head.y-1 === tail.y || head.y+1 === tail.y)
+function moveTail() {
+    for(let knotIndex = 1; knotIndex < rope.length; knotIndex++) {
+        if(!knotsAreTouching(rope[knotIndex-1], rope[knotIndex])) {
+            moveKnot(rope[knotIndex-1], rope[knotIndex]);
+        }
+    }
 }
+
+function moveKnot(headKnot, knot) {
+    var xChange = 0, yChange = 0;
+
+    if(headKnot.x !== knot.x && headKnot.y !== knot.y) { // diagonally
+        xChange = headKnot.x > knot.x ? 1 : -1;
+        yChange = headKnot.y > knot.y ? 1 : -1;
+    } else if(headKnot.y === knot.y) { // same row
+        xChange = headKnot.x > knot.x ? 1 : -1;
+    } else if (headKnot.x === knot.x) {
+        yChange = headKnot.y > knot.y ? 1 : -1;
+    }
+
+    knot.x += xChange;
+    knot.y += yChange;
+
+    if(knot === rope[rope.length-1]) {
+        if(!visitedPositions.find(pos => pos.x === knot.x && pos.y === knot.y)) {
+            visitedPositions.push({x: knot.x, y:knot.y})
+            numberOfVisitedPositions++;
+        }
+    }
+}
+
+function knotsAreTouching(headKnot, knot) {
+    return (headKnot.x === knot.x || headKnot.x-1 === knot.x || headKnot.x+1 === knot.x)
+        && (headKnot.y === knot.y || headKnot.y-1 === knot.y || headKnot.y+1 === knot.y);}
 
 moves.forEach(move => {
     let splitMove = move.split(" ");
     let direction = splitMove[0];
-    let movesToBeDone = splitMove[1];
+    let movesToBeDone = Number(splitMove[1]);
 
     for (let movesDone = 0; movesDone < movesToBeDone; movesDone++) {
-        let oldHeadPosition = {x:head.x, y:head.y};
         moveHead(direction);
-        if(!headTouchesTail()) {
-            tail = oldHeadPosition;
-            if(!visitedPositions.find(position => position.x === tail.x && position.y === tail.y)) {
-                visitedPositions.push(oldHeadPosition);
-                numberOfVisitedPositions++;
-            }
-        }
+        moveTail();
     }
-})
+});
+
 
 result1div.innerText = numberOfVisitedPositions;
