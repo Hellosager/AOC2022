488cb687c5478bcdd479003b6ece0748b5b12616
Hellosager <sebas.b@web.de>
2022-12-10 12:02:31 +0100

    solve day 10

---
+++ Day10/solution.js
@@ -0,0 +1,44 @@
+const operations = input.split(/\n/);
+// const operations = demoInput.split(/\n/);
+var currentCycle = 0;
+var regX = 1;
+
+var signal = 0;
+var signalOutput = "";
+
+function checkForSignal() {
+    if (currentCycle <= 220 && currentCycle % 40 === 20) {
+        signal += (currentCycle * regX);
+    }
+}
+
+function cycle() {
+    currentCycle++;
+    let crtPosition = currentCycle % 40 === 0
+        ? 39
+        : (currentCycle % 40)-1;
+    signalOutput += (crtPosition >= regX-1 && crtPosition <= regX+1)
+        ? "#"
+        : " . ";
+    if(currentCycle % 40 === 0) {
+        signalOutput += "\n";
+    }
+}
+
+operations.forEach(operation => {
+    if (operation === "noop") {
+        cycle();
+        checkForSignal();
+    } else {
+        let regV = Number(operation.match(/-?\d+/)[0]);
+        cycle();
+        checkForSignal();
+        cycle();
+        checkForSignal();
+        regX += regV;
+        console.log(regX);
+    }
+});
+
+result1div.innerText = signal;
+result2div.innerText = signalOutput;
