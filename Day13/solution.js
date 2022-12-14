2f3a158cecbd197869f668aeae4ef4de3fc35235
Sebastian Beck - dotSource GmbH <sbc@intranet.dotsource.de>
2022-12-14 11:31:47 +0100

    WIP day 13

--- Day13/solution.js
+++ Day13/solution.js
@@ -2,7 +2,66 @@
 const inputSplitByPackagePairs = demoInput.split(/\n\n/);
 var packagePairs = [];
 inputSplitByPackagePairs.forEach(inputPackagePair => {
-   let packageLines = inputPackagePair.split(/\n/);
-    packagePairs.push({leftList:eval(packageLines[0]), rightList:eval(packageLines[1])});
+    let packageLines = inputPackagePair.split(/\n/);
+    packagePairs.push({leftList: eval(packageLines[0]), rightList: eval(packageLines[1])});
 });
 
+var sumOfIndices = 0;
+
+function isOrdered(leftValue, rightValue) {
+    // console.log("left: " + leftValue + ", right: " + rightValue);
+
+    // one of them is integer, try again as list
+    if (Number.isInteger(leftValue) && !Number.isInteger(rightValue)) {
+        return isOrdered([leftValue], rightValue);
+    } else if (!Number.isInteger(leftValue) && Number.isInteger(rightValue)) {
+        return isOrdered(leftValue, [rightValue]);
+    }
+
+    // both are integers, compare them
+    if (Number.isInteger(leftValue) && Number.isInteger(rightValue)) {
+        return leftValue <= rightValue;
+    }
+
+
+    // if(leftValue.length > rightValue.length) {
+    //     return false;
+    // } else if(leftValue.length === 0) {
+    //     return true;
+    // }
+
+    // both are lists, compare each value of left with right
+    // var ordered = true;
+    for (let i = 0; i < (Math.max(leftValue.length, rightValue.length)); i++) {
+        if(leftValue[i] === undefined && rightValue[i] !== undefined) { // left ran out first
+            return true;
+        } else if (leftValue[i] !== undefined && rightValue[i] === undefined) { // right ran out first
+            return false;
+        }
+
+        if(leftValue[i] === rightValue[i]) {
+
+        } else {
+            // if (!isOrdered(leftValue[i], rightValue[i])) {
+                return isOrdered(leftValue[i], rightValue[i]);
+            // }
+        }
+
+    }
+
+    return true;
+}
+
+for (let i = 0; i < packagePairs.length; i++) {
+    {
+        console.log("Checking pair " + (i+1));
+        if (isOrdered(packagePairs[i].leftList, packagePairs[i].rightList)) {
+            console.log("pair " + (i+1) + " is ordered.");
+            sumOfIndices += i+1;
+        }
+    }
+
+}
+
+result1div.innerText = sumOfIndices;
+// 5546 is too high
